97e9317c19f987eb29c8ae1866452993ad2d03fa
PappNiki20 <papp.nikoletta001@gmail.com>
2023-04-18 16:23:46 +0200

    modal probalkozas, nem jelenik meg

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ import { ADATLISTA } from "./adat.js";
 import { kulcsLista } from "./adat.js";
 import { rendezes } from "./rendezesek.js";
 $(function () {
+  init();
+});
+function init() {
   const ARTICLEELEM = $("article");
   let txt = osszeallit(ADATLISTA);
   megjelenit(txt, ARTICLEELEM);
-});
+  
+  
+}
 
 function osszeallit(lista) {
   let txt ='<div class="kozep">'
@@ -17,11 +22,29 @@ function osszeallit(lista) {
     </div>
     <img class="card-img-bottom" src="${lista[index].kep}" alt="Card image" style="width:100%">
     <p class="card-text">${lista[index].kor}, ${lista[index].fajta}</p>
-    <button class="btn btn-primary">Mutat</button>
+    <button  class="btn btn-primary modalBtn" data-toggle="modal" data-target="#myModal">Mutat</button>
     </div>`;
   }
   txt += "</div>";
   txt += "</div>";
+  let modal = $("<div>").addClass("modal").attr("id", "myModal");
+  let modalContent = $("<div>").addClass("modal-content");
+  let closeBtn = $("<span>").addClass("close").attr("data-dismiss", "modal").html("&times;");
+  let modalText = $("<p>").text("Ez egy modal ablak!");
+
+  modalContent.append(closeBtn, modalText);
+  modal.append(modalContent);
+  $("body").append(modal);
+
+  // Gomb eseménykezelő hozzárendelése
+  $(".modalBtn").click(function() {
+    $("#myModal").modal("show");
+  });
+
+  // Bezárás gomb eseménykezelő hozzárendelése
+  $(".close").click(function() {
+    $("#myModal").modal("hide");
+  });
   return txt;
 }
 function megjelenit(adat, tarolo) {
