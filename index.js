a1c467f79400e135a3329c3a0d5f13ed4960f765
PappNiki20 <papp.nikoletta001@gmail.com>
2023-04-20 15:32:47 +0200

    modal ujra

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ADATLISTA } from "./adat.js";
-let modalIndex = 0;
+import { kulcsLista } from "./adat.js";
+import { rendezes } from "./rendezesek.js";
 $(function () {
   init();
 });
@@ -7,63 +8,43 @@ function init() {
   const ARTICLEELEM = $("article");
   let txt = osszeallit(ADATLISTA);
   megjelenit(txt, ARTICLEELEM);
-  const GOMB = $(".mod");
-  const MODAL = $("#modbod");
-  console.log(GOMB);
-
-  GOMB.on("click", function () {
-    MODAL.html(modaltartalom(this.id));
-    modalIndex = this.id;
-  });
-
-  const BAL = $("#bal");
-
-  BAL.on("click", function () {
-    modalIndex--;
-    if (modalIndex < 0) {
-      modalIndex = ADATOK.length - 1;
-    }
-    MODAL.html(modaltartalom(modalIndex));
-  });
-
-  const JOBB = $("#jobb");
-
-  JOBB.on("click", function () {
-    modalIndex++;
-    if (modalIndex > ADATOK.length - 1) {
-      modalIndex = 0;
-    }
-    MODAL.html(modaltartalom(modalIndex));
-  });
+  
+  
 }
 
 function osszeallit(lista) {
-  let txt = '<div class="kozep">';
-  txt += '<div class="container mt-4 row">';
+  let txt ='<div class="kozep">'
+   txt += '<div class="container mt-4 row">';
   for (let index = 0; index < lista.length; index++) {
     txt += `<div class="card" class="card col-lg-3 col-md-4 col-sm-6 p-0" style="width:400px">
-    <div class="card-body"> <h4 class="card-title">${lista[index].nev}<br></h4>
+    <div class="card-body"> <h4 class="card-title">${lista[index].nev}</h4>
     
     </div>
     <img class="card-img-bottom" src="${lista[index].kep}" alt="Card image" style="width:100%">
-    <p class="card-text">Kor: ${lista[index].kor} <br> Fajta:  ${lista[index].fajta}<br></p>
-    </p><button id= "${index}" type="button" class="btn btn-primary mod fekete" data-bs-toggle="modal" data-bs-target="#myModal">Mutat</button>
+    <p class="card-text">${lista[index].kor}, ${lista[index].fajta}</p>
+    <button  class="btn btn-primary modalBtn" data-toggle="modal" data-target="#myModal">Mutat</button>
     </div>`;
   }
   txt += "</div>";
   txt += "</div>";
+  let modal = $("<div>").addClass("modal").attr("id", "myModal");
+  let modalContent = $("<div>").addClass("modal-content");
+  let closeBtn = $("<span>").addClass("close").attr("data-dismiss", "modal").html("&times;");
+  let modalText = $("<p>").text("Ez egy modal ablak!");
 
-  return txt;
-}
-function modaltartalom(szam) {
-  let txt = "";
-  txt +=
-    "<h2>" +
-    ADATLISTA[szam].nev +
-    "</h2><p><b>Kor:</b> " +
-    ADATLISTA[szam].kor +
-    "<br><b>Fajta: </b>";
-  ("</p>");
+  modalContent.append(closeBtn, modalText);
+  modal.append(modalContent);
+  $("body").append(modal);
+
+  // Gomb eseménykezelő hozzárendelése
+  $(".modalBtn").click(function() {
+    $("#myModal").modal("show");
+  });
+
+  // Bezárás gomb eseménykezelő hozzárendelése
+  $(".close").click(function() {
+    $("#myModal").modal("hide");
+  });
   return txt;
 }
 function megjelenit(adat, tarolo) {
